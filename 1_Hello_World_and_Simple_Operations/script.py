4c8bd305710620cbc8718a664b8ed65aa1076709
User <user@localhost.localdomain>
2016-03-28 18:40:03 +0100

    Python Commit #1
    Python Commit #1 - print

---
+++ 1_Hello_World_and_Simple_Operations/script.py
@@ -0,0 +1,8 @@
+#Description: Simple script to print "Hello World" and make some numerical operations 
+#Author Nicolas (nicolas.mail@mailcom)
+
+print('Hello World')
+
+print("Hello World")
+
+print('Spam and eggs...')
