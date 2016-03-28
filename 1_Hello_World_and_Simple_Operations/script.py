a3586a1832fca4e6de14d56c91a1b6c78986c98b
User <user@localhost.localdomain>
2016-03-28 18:57:54 +0100

    Python Commit #2
    Python Commit #2 - print and numbers operations

--- 1_Hello_World_and_Simple_Operations/script.py
+++ 1_Hello_World_and_Simple_Operations/script.py
@@ -6,3 +6,15 @@ print('Hello World')
 print("Hello World")
 
 print('Spam and eggs...')
+
+print('2 + 2 = ')
+2 + 2
+
+print('3 - 2 = ')
+3 - 2
+
+print('4 * 4 = ')
+4 * 4
+
+print('5 / 4 = ')
+5 / 4
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python-sololearn
+# This is a repository which registers some Python practice.
