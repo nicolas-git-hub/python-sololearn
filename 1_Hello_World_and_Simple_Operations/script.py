568b3c229da012bf3e1f5d90774c5fd0230dd9db
nicolas-git-hub <nicolas.mail@mail.com>
2016-03-28 19:53:32 +0100

    New lines
    New lines for readability

--- 1_Hello_World_and_Simple_Operations/script.py
+++ 1_Hello_World_and_Simple_Operations/script.py
@@ -2,27 +2,36 @@
 # Author Nicolas (nicolas.mail@mailcom)
 
 print('Hello World')
+print("\n")
 
 print("Hello World")
+print("\n")
 
 print('Spam and eggs...')
+print("\n")
 
 print('2 + 2 = ')
 print(2 + 2)
+print("\n")
 
 print('3 - 2 = ')
 print(3 - 2)
+print("\n")
 
 print('4 * 4 = ')
 print(4 * 4)
+print("\n")
 
 print('5 / 4 = ')
 print(5 / 4)
+print("\n")
 
 # More operations
 
 print("2 * (3 + 4) = ")
 print(2 * (3 + 4))
+print("\n")
 
 print("10 / 2 = ")
 print(10 / 2)
+print("\n")
