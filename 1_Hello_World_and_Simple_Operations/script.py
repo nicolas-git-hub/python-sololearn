f4583730a6683764911caa86402031732c51bb51
nicolas-git-hub <nicolas.mail@mail.com>
2016-03-28 19:47:34 +0100

    More operations
    Operations with parenthesis

--- 1_Hello_World_and_Simple_Operations/script.py
+++ 1_Hello_World_and_Simple_Operations/script.py
@@ -1,5 +1,5 @@
-#Description: Simple script to print "Hello World" and make some numerical operations 
-#Author Nicolas (nicolas.mail@mailcom)
+# Description: Simple script to print "Hello World" and make some numerical operations 
+# Author Nicolas (nicolas.mail@mailcom)
 
 print('Hello World')
 
@@ -18,3 +18,11 @@ print(4 * 4)
 
 print('5 / 4 = ')
 print(5 / 4)
+
+# More operations
+
+print("2 * (3 + 4) = ")
+print(2 * (3 + 4))
+
+print("10 / 2 = ")
+print(10 / 2)
