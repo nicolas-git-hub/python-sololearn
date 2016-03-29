4f32cd573fd1e7cb012f992b88b12eb3001ac27c
nicolas-git-hub <nicolas.mail@mail.com>
2016-03-29 20:54:56 +0100

    Error examples on operation
    More error examples on division by zero

--- 1_Hello_World_and_Simple_Operations/script.py
+++ 1_Hello_World_and_Simple_Operations/script.py
@@ -42,3 +42,20 @@ print(-7)
 print("\n")
 print("(-7 + 2) * (-4) =")
 print((-7 + 2) * (-4))
+
+"""
+# Example of an error (Division by zero)
+
+print("\n")
+print("11 / 0")
+print(11 / 0)
+"""
+
+# Another example of error produced by a division by zero
+
+print("\n")
+print("17 + 94) / (-5 + 5) =")
+print((17 + 94) / (-5 + 5))
+
+# The script stops on the first error.
+# "The second error only ocurrs if the first one is commented.
