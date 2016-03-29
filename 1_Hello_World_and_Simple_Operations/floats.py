d7c3f9d01a1d395898fba900f970b968ef7a4099
nicolas-git-hub <nicolas.mail@mail.com>
2016-03-29 21:30:09 +0100

    Mpre operations on floats
    Mpre operations on floats

--- 1_Hello_World_and_Simple_Operations/floats.py
+++ 1_Hello_World_and_Simple_Operations/floats.py
@@ -7,3 +7,20 @@ print("\n")
 print("9 + .8765000 =")
 print(9 + .85765000)
 print("\n")
+
+"""
+More operations with floats
+Here Python converts integer into floats in a implicit way
+but usually you have to do it manually
+"""
+
+print(8 / 2)
+print("\n")
+
+print(6 * 7.0)
+print("\n")
+
+print(4 + 1.65)
+print("\n")
+
+
