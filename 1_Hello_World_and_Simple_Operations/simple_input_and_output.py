b79dfd674bc181a0596bd1971813130cc8a747f5
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-04 21:10:14 +0100

    Corrections
    Corrections

--- 1_Hello_World_and_Simple_Operations/simple_input_and_output.py
+++ 1_Hello_World_and_Simple_Operations/simple_input_and_output.py
@@ -3,3 +3,9 @@
 print(1 + 1)
 print("\n")
 print("Hello\nWorld!")
+
+# To get an input from the user, use "input" method.
+# The function prompts the user an input and returns what was entered as 
+# a string with the content already escaped.
+
+input("Enter something please: ")
