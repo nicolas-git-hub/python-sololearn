0e6099b85aacfd9cc5159be1195517e143f84e04
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-11 21:48:38 +0100

    Control structure
    Nested if

--- 2_Control_Structures/if.py
+++ 2_Control_Structures/if.py
@@ -4,3 +4,20 @@ if 10 > 5:
 	print("10 is greater than 5")
 	
 print("Program ended")
+
+
+# Nested if
+
+num = 12
+if num > 5:
+	print("Bigger than 5")
+	if num <= 47:
+		print("Between 5 and 47")
+
+
+num = 48
+if num > 5:
+	print("Bigger than 5")
+	if num <= 47:
+		print("Between 5 and 47")
+
