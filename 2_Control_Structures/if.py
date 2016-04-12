64a74c05a0a8ef8e18dc4c62de6dc02271964962
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-12 13:30:45 +0100

    Control Structure
    If, else

--- 2_Control_Structures/if.py
+++ 2_Control_Structures/if.py
@@ -28,3 +28,19 @@ if x == 5:
 	print("Yes")
 else:
 	print("No")
+
+
+# If, else chain
+
+num = 5
+if num == 5:
+	print("Number is 5")
+else:
+	if num == 11:
+		print("Number is 11")
+	else:
+		if num == 7:
+			print("Number is 7")
+		else:
+			print("Number isn't 5, 11 or 7")
+
