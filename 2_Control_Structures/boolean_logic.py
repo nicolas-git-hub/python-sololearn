aa83dacfef65fe2b1f5020d2390faaf92cb48618
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-12 17:07:27 +0100

    If and Boolean Logic
    Observation on Boolean Logic in Python

---
+++ 2_Control_Structures/boolean_logic.py
@@ -0,0 +1,6 @@
+# Python uses words instead of symbols like &&, || and !
+
+print(1 == 1 and 2 == 2)
+print(1 == 1 and 2 == 3)
+print(1 != 1 and 2 == 2)
+print(2 < 1 and 3 > 6)
--- 2_Control_Structures/if.py
+++ 2_Control_Structures/if.py
@@ -44,3 +44,14 @@ else:
         else:
             print("Number isn't 5, 11 or 7")
 
+# Elif statement (shortcut for "else if")
+
+num = 6
+if num == 5:
+    print("Number is 5")
+elif num == 11:
+    print("Number is 11")
+elif num == 7:
+    print("Number is 7")
+else:
+    print("Number isn't 5, 11 or 7")
