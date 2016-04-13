39aa7e3316384eb2862ab389ab7b96a59e80a886
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-13 21:11:57 +0100

    Lists in Python
    Lists and indexation

---
+++ 2_Control_Structures/lists.py
@@ -0,0 +1,11 @@
+"""
+Lists are another type of object in Python
+They store an indexed list of items
+Lists are created using the square brackets and commas
+To access an certain item of a list, use its index in the squere brackets
+"""
+
+words = ["Hello", "world", "!"]
+print(words[0])
+print(words[1])
+print(words[2])
