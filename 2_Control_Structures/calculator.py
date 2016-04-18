f99867d293454872bd0db322992b26341c8a8901
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-19 00:45:29 +0100

    Simple Calculator
    A simple calmulator example

---
+++ 2_Control_Structures/calculator.py
@@ -0,0 +1,39 @@
+"""
+A simple calculator in Python.
+
+First section is the menu.
+This keeps on accepting user inputs until the user enters "quit", so
+a while loop is used.
+"""
+
+while True:
+    print("Options:")
+    print("Enter 'add' to add two numbers")
+    print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to multiply two numbers")
+    print("Enter 'divide' to divide two numbers")
+    print("Enter 'quit' to quit the program")
+    user_input = input(": ")
+
+    if user_input == "quit":
+        break
+    elif user_input == "add":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 + num2)
+    elif user_input == "subtract":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 - num2)
+    elif user_input == "multiply":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 * num2)
+    elif user_input == "divide":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 / num2)
+    else:
+        print("Unknown input")
+        
+    print("The answer is " + result)
