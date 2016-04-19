72d4a96bdaf524f2d43e8122ebeaa3d874ad46f8
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-19 14:36:47 +0100

    Comments and Functions
    More on comments and starting Functions in Python

--- 3_Functions_and_modules/comments.py
+++ 3_Functions_and_modules/comments.py
@@ -13,3 +13,29 @@ print(x % y) # find the remainder
 # print (x // y)
 # another comment
 
+
+"""
+Docstrings:
+
+Docstrings (documentation strings) serve a similar purpose to comments,
+as they are designed to explain code. However, they are more specific
+and have a different syntax.
+They are created by putting a multiline string containing an explanation
+of the function below the function's name first line.
+"""
+
+def shout(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print(word + "!")
+
+shout("spam")
+
+"""
+Unlike conventional comments, docstrings are
+retained thoughout the runtime of the program.
+This allows the programmer to inspect these
+comments at run time.
+"""
---
+++ 3_Functions_and_modules/functions_as_variables.py
@@ -0,0 +1,22 @@
+"""
+Although they are created differently from normal variables,
+functions are just like any other kind of value.
+They can be assigned and reassigned to variables,
+and later referenced by those names.
+"""
+
+def multiply(x, y):
+    return x * y
+
+a = 4
+b = 7
+
+operation = multiply
+
+print(operation(a, b))
+
+"""
+The example above assigned the function multiply to
+a variable operation. Now, the name operation can also
+be used to call the function.
+"""
