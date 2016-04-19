98a09c184d1acd41ad4fa1508aba296884acce50
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-19 12:54:36 +0100

    Functions and arguments
    Functions and arguments in Python

---
+++ 3_Functions_and_modules/functions.py
@@ -0,0 +1,22 @@
+"""
+Any statement that consists of a word followed by information in
+parentheses is a function call.
+
+Examples:
+"""
+
+print("Hello World!")
+range(2, 20)
+str(12)
+range(10, 20, 3)
+
+print("\n")
+print(list(range(2, 20)))
+print(str(12))
+print(list(range(2, 20, 3)))
+
+"""
+The words in front fo the parentheses are the function names, and the
+comma-separated values inside the perentheses are function arguments.
+"""
+
