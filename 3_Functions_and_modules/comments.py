5fd12999094c3330f1e8969bdeca4467d536a00b
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-19 13:44:54 +0100

    Comments
    Using # to comment code

---
+++ 3_Functions_and_modules/comments.py
@@ -0,0 +1,15 @@
+"""
+Comments are annotations to code used to make easier to understand.
+They don't affect how code is run.
+In Python, a comment is created y inserting an octothorpe (#).
+All the text after it on that line is ignored
+"""
+
+x = 365
+y = 7
+# this is a comment
+
+print(x % y) # find the remainder
+# print (x // y)
+# another comment
+
