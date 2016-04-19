41bf5ab3b066b89b8209d0bf17d3ae777ea3de8b
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-20 00:49:53 +0100

    Raising Exceptions
    Raising exceptions inside and outside try/except blocks

--- 4_Exceptions_and_Files/raising_exceptions.py
+++ 4_Exceptions_and_Files/raising_exceptions.py
@@ -3,7 +3,22 @@
 # You can raise exceptions by using the raise statement.
 
 print(1)
-raise ValueError
+#raise ValueError <- had to be commented.
 print(2)
 
 # You need to specify the type of the exception raised.
+
+# Exceptions can be raised with arguments that give detail about them.
+
+name = "123"
+#raise NameError("Invalid name!") <- had to be commented.
+
+#
+# In except blocks, the raise statement can be used without arguments
+# to re-raise whatever exception occoured.
+
+try:
+    num = 5 / 0
+except:
+    print("An error accoured")
+    raise
