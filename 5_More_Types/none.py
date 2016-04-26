d34d263b6255ad40157feafbfafe79cc8e2f4429
nicolas-git-hub <nicolas.mail@mail.com>
2016-04-26 13:39:31 +0100

    Type None
    Finishing Type None file

--- 5_More_Types/none.py
+++ 5_More_Types/none.py
@@ -12,3 +12,29 @@ print(bool(None) == bool(""))
 print(bool(None) == False)
 
 # Output: True
+
+# Considerations:
+# None is an absence of a value
+# None object is returned by any function that doesn't return anything else
+#
+# Consider:
+
+def some_func():
+    print("Hi")
+
+var = some_func()
+print(var)
+
+# This outputs "Hi" and "None"
+
+# ==============================
+
+# Another example:
+
+foo = print()
+if foo == None:
+    print(1)
+else:
+    print(2)
+
+# It outputs "1" because function print() returns None
